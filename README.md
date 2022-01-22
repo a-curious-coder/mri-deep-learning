7142527ab063428d270604f0440d7e75a095716c
zardoss <callummclennan1@googlemail.com>
2022-01-22 14:16:09 +0000

    Print columns with nan values/ Update README

--- README.md
+++ README.md
@@ -21,3 +21,20 @@ Run the python file
 ```bash
 python main.py
 ```
+<!-- ## What is Alzheimer's Disease?p -->
+
+## Exploratory Data Analysis
+Classification of Alzheimer's Disease from the MRI datasets is a binary classification problem as the patients either have Alzheimer's Disease (AD) or they are Cognitive Normal (CN).
+
+Each patient is represented by a unique Security ID (SID). Each patient has had at least 2 scan sessions, thus has 2 or more MRI scans. In the effort of dimensionality reduction, I should try and represent each patient within a single row.
+<!-- Pie chart for Diagnosis to represent binary ratio -->
+<!--  Will Cross Validation be needed due to size/ratio of diagnosis -->
+<!-- Something to do with Age -->
+
+## Process
+This section will outline my thoughts behind what I've done and why I've done it for classifying the various types of data.
+### Tabular Data (ADNI/IXI)
+I've stored the MRI tabular data in Amazon's AWS cloud storage for safe keeping. Using various API keys, I can securely load in the data to this python script.
+
+Before the data is given to a deep learning model, I need to clean and simplify the data such that there's enough data there to make a classification but not too much it's too much. 
+
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import boto3
 import os
 import pandas as pd
-
+import plotly
 from dotenv import load_dotenv
 from sklearn.decomposition import PCA
+
+def print_data_shape(data):
+    print(f"This dataset has {data.shape[0]} rows and {data.shape[1]} columns")
+
+
+def print_null_values(data):
+    """ Prints the quantity of null/NaN values in columns containing null/NaN values
+
+    Args:
+        data (pd.DataFrame): Tabular MRI data
+    """
+    # Columns containing null/NaN values
+    null_column_names = data.columns[data.isnull().any()].tolist()
+    print(f"There are {len(null_column_names)} columns with null/NaN values")
+    for column in null_column_names:
+        print(f"{column}: {data[column].isna().sum()}")
+
 
 def normalize_tabular_data(mri_data):
     """Normalizes the values of each column (Excluding columns unrelated to the MRI scan itself)
@@ -56,24 +73,29 @@ def main():
                           aws_access_key_id=access_key,
                           aws_secret_access_key=secret_access_key)
 
-    print("-------------------\nLoad Data\n -------------------")
+    print("-------------------\nLoad Data\n-------------------")
     mri_data = get_tabular_data(client)
-    print("-------------------\nNormalize Data\n -------------------")
+    # TODO: Get rid of to_csv line
+    print_data_shape(mri_data)
+    print("-------------------\nExploratory Data Analysis\n-------------------")
+    # TOOD: Plotly Dashboard of sorts which will be saved as HTML
+    print("-------------------\nNormalize Data\n-------------------")
     normalized_mri_data = normalize_tabular_data(mri_data)
+    print_null_values(normalized_mri_data)
     # NaN values occur because there are a bunch of columns filled with zeros, thus can't be normalized
     normalized_mri_data = normalized_mri_data.fillna(0)
-    print("-------------------\nFiltered Data\n -------------------")
+    print("-------------------\nFiltered Data\n-------------------")
     # Filter data to remove last 6 columns (irrelevant)
     filtered_data = normalized_mri_data.iloc[:, :normalized_mri_data.shape[1]-6]
     # Checks if any values are NaN
     nan_values = filtered_data.isna()
     nan_columns = nan_values.any()
     columns_with_nan = filtered_data.columns[nan_columns].tolist()
-    print(columns_with_nan)
-    print("-------------------\nPCA\n -------------------")
+    # print(columns_with_nan)
+    print("-------------------\nPCA\n-------------------")
     pca = PCA(n_components=2)
     pca.fit(filtered_data)
-    print(pca.components_)
+    # print(pca.components_)
 
 
 if __name__ == "__main__":
